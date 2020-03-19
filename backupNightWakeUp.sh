c1bd41e69793f53726312c98401d3eb328a0f5b2
Gregory Brancq <gregory.brancq@free.fr>
2020-03-19 10:02:28 +0100

    Added file to execute the program at wake up

---
+++ backupNightWakeUp.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# this script must be linked here by root user
+# ln -s /home/greg/Greg/work/env/projects/backupNight/backupNightWakeUp.sh /lib/systemd/system-sleep/backupNightWakeUp
+
+export DISPLAY=:0.0
+export XAUTHORITY=/home/greg/.Xauthority
+
+case $1 in
+    post)
+        echo "Execute backupNight script from /lib/systemd/system-sleep"
+        /home/greg/Greg/work/env/bin/backupNight
+        ;;
+esac
