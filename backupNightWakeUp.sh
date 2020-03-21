77914f529bfb081252ee51b0f6fa2b0313cf621f
Gregory Brancq <gregory.brancq@free.fr>
2020-03-21 10:38:44 +0100

    Corrected link path
    Change-Id: I66b3893838e1328fe0a9b0a71bc88fc140bb92e1

--- backupNightWakeUp.sh
+++ backupNightWakeUp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # this script must be linked here by root user
-# ln -s /home/greg/Greg/work/env/projects/backupNight/backupNightWakeUp.sh /lib/systemd/system-sleep/backupNightWakeUp
+# ln -s /home/greg/Greg/work/env/backupNight/backupNightWakeUp.sh /lib/systemd/system-sleep/backupNightWakeUp
 
 export DISPLAY=:0.0
 export XAUTHORITY=/home/greg/.Xauthority
