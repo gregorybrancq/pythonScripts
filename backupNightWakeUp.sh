3b9f6cde25e3d9bba9ce44213dba9fbc2a301174
Gregory Brancq <gregory.brancq@free.fr>
2020-03-22 11:13:43 +0100

    Added systemd-sleep issue
    Change-Id: Ide4b81fa9ed0d637111a0d3cc70bddfc0d9ff9d0

--- backupNightWakeUp.sh
+++ backupNightWakeUp.sh
@@ -2,6 +2,11 @@
 
 # this script must be linked here by root user
 # ln -s /home/greg/Greg/work/env/backupNight/backupNightWakeUp.sh /lib/systemd/system-sleep/backupNightWakeUp
+#
+# I encountered an issue with systemd, once the program was launched, it was killed by systemd-sleep after 90s
+#   systemd-sleep: (sd-executor) terminated by signal ALRM
+# linked to systemd-cron program (https://bugs.python.org/issue26839)
+# the strange thing was that it was not installed... anyway I installed it and removed it, and it works.
 
 export DISPLAY=:0.0
 export XAUTHORITY=/home/greg/.Xauthority
