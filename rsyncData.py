576784eeb3f59baaddb3dc87bdba13fb116600a7
Gregory Brancq <gregory.brancq@free.fr>
2020-05-17 16:11:12 +0200

    Make program more generic. Construct command depends on tool. Can be launched on portable or on home computers.
    Change-Id: Idce0a722cbfda4c9b8ddf2998422fcc3b3e848f5

--- rsyncData.py
+++ rsyncData.py
@@ -12,7 +12,7 @@ sys.path.append('/home/greg/Greg/work/env/pythonCommon')
 from basic import getConfigDir
 from log_and_parse import createLog, parsingLine
 from program import Program
-from network import checkAddress
+from network import checkAddress, getIp
 
 ##############################################
 # Global variables
@@ -20,51 +20,79 @@ from network import checkAddress
 
 progName = 'rsyncData'
 
-# configuration files
-configFile = os.path.join(getConfigDir(), progName, progName + ".cfg")
-
 # data
 data = "/media/perso/data/"
 remote = "greg@home:"
-thunderbird_source = os.path.join(data, "thunderbird", "Portable", "*")
-thunderbird_target = remote + os.path.join(data, "thunderbird", "Portable")
-firefox_source = os.path.join(data, "firefox", "Portable", "*")
-firefox_target = remote + os.path.join(data, "firefox", "Portable")
 
 # flag
 error = False
+pc = None
 
 
 ##############################################
 # Functions
 ##############################################
 
-def rsyncData(src, dst):
+def rsyncData(param):
     global error
-    logger.debug("RsyncData from %s to %s" % (src, dst))
-    cmd = 'rsync -rulpgvz --delete -e "ssh -p 2832" ' + src + ' ' + dst
-    logger.debug("RsyncData cmd = %s" % str(cmd))
-    proc = subprocess.Popen(cmd, stderr=subprocess.STDOUT, shell=True)
-    proc.wait()
-    if proc.returncode != 0:
-        error = True
-        logger.error("Error during rsync data from %s to %s" % (src, dst))
+    src = param[0]
+    dst = param[1]
+    logger.info("RsyncData from %s to %s" % (src, dst))
+
+    # construct command
+    cmd = 'rsync -rulpgvz --delete -e '
+    if pc == "Portable":
+        cmd += '"ssh -p 2832" '
+    cmd += src + ' ' + dst
+
+    logger.info("RsyncData cmd = %s" % str(cmd))
+    if not parsedArgs.dryRun :
+        proc = subprocess.Popen(cmd, stderr=subprocess.STDOUT, shell=True)
+        proc.wait()
+        if proc.returncode != 0:
+            error = True
+            logger.error("Error during rsync data from %s to %s" % (src, dst))
+
+
+def getDirName(tool):
+    """ get the directories names of source and target"""
+    tool_source = os.path.join(data, tool, pc, "*")
+    tool_target = str()
+    if pc == "Portable":
+        tool_target += remote
+    tool_target += os.path.join(getConfigDir(), tool, pc)
+    logger.debug("tool_source = %s, tool_target = %s" % (tool_source, tool_target))
+    return tool_source, tool_target
 
 
 def copyLocalData():
     """ copy local data to home directory """
-    logger.info("Copy thunderbird data from %s to %s" % (thunderbird_source, thunderbird_target))
-    rsyncData(thunderbird_source, thunderbird_target)
+    logger.info("Copy thunderbird data")
+    rsyncData(getDirName("thunderbird"))
 
-    logger.info("Copy firefox data from %s to %s" % (firefox_source, firefox_target))
-    rsyncData(firefox_source, firefox_target)
+    logger.info("Copy firefox data")
+    rsyncData(getDirName("firefox"))
 
 
 def main():
+    global pc
     logger.info("START")
 
+    # determine on which pc this program is launched
+    my_ip = getIp()
+    if my_ip == "192.168.1.101":
+        pc = "Home"
+    elif my_ip == "192.168.1.103":
+        pc = "Portable"
+    else:
+        logger.exit("Can't determine the pc.")
+        sys.exit(1)
+
+    # config file name
+    config_file = os.path.join(getConfigDir(), progName, progName + "_" + pc + ".cfg")
+
     # program management
-    program = Program(prog_name=progName, config_file=configFile)
+    program = Program(prog_name=progName, config_file=config_file)
 
     # be sure that synchronisation is not running
     if not program.isRunning():
@@ -75,7 +103,7 @@ def main():
             if checkAddress("192.168.1.101"):
                 # Rsync local data
                 copyLocalData()
-                if not error:
+                if not error and not parsedArgs.dryRun:
                     # Update config file
                     program.runToday()
         program.stopRunning()
